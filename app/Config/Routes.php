364b52af5f3d9d1c933224ee96ab236a39b53ed8
SryaPtra <suryauyawangon@gmail.com>
2025-06-23 09:27:21 +0700

    pengguna favorit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,10 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->resource('resep');
 $routes->resource('Pengguna');
+// $routes->resource('penggunafavorit');
+$routes->group('penggunafavorit', function($routes) {
+    $routes->get('(:num)', 'PenggunaFavorit::index/$1');
+    $routes->post('/', 'PenggunaFavorit::create');
+    $routes->delete('(:num)/(:num)', 'PenggunaFavorit::delete/$1/$2');
+});
+
