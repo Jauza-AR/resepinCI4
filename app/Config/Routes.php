320aaa379d377a11c31ee46c1841fbf603fabf64
Zhiya Ulhaq <zhiyaulhaq25@gmail.com>
2025-06-29 22:00:21 +0700

    menambahkan routes baru, mengganti bio di penggunamodel, menambah controller di pengguna dan resep favorit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,7 +34,6 @@ $routes->group('penggunafavorit', function($routes) {
 });
 
 $routes->resource('pengguna');
-
 $routes->resource('resepfavorit');
 
 $routes->get('komentar/resep/(:num)', 'Komentar::byResep/$1');
@@ -42,9 +41,13 @@ $routes->resource('komentar');
 
 $routes->post('upload-foto', 'UploadController::uploadFoto');
 $routes->post('reseplike/like', 'ResepLike::likeResep');
+
 $routes->post('favorite/add', 'ResepFavorit::addToFavorites');
 $routes->post('favorite/remove', 'ResepFavorit::removeFromFavorites');
+$routes->post('favorite/check', 'ResepFavorit::check');
 
 
 $routes->delete('favorite/remove', 'ResepFavorit::removeFromFavorites');
+// $routes->delete('favorite/user/(:num)/resep/(:num)', 'ResepFavorit::removeFromFavorites/$1/$2');
+
 
